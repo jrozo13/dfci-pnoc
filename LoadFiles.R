3d3d19df67093495537fbb3c83d5f4dfdaf15a26
jrozo13 <jrozowsk@u.rochester.edu>
2022-07-13 15:39:12 -0400

    Updates 7/13

--- LoadFiles.R
+++ LoadFiles.R
@@ -3,30 +3,44 @@
 
 ########## Initialize ##########
 ## Install global packages ##
-library(dplyr)
-library(tidyr)
 library(tidyverse)
-library(readxl)
-library(RColorBrewer)
-library(circlize)
-library(ggplot2)
-library(ggrepel)
-col_annotations <- list(
-  Sex = c("M" = "skyblue", "F" = "pink"),
-  Location = c("Spinal" = "#EF553B", 
-               "Posterior fossa" = "#636EFA", 
-               "Supratentorial" = "#00CC96"),
-  Age = colorRamp2(c(0,20), c("#edf8e9", "#74c476"))
-)
+library(org.Hs.eg.db)
 
-wd <- "/Users/jrozowsky/Library/Mobile Documents/com~apple~CloudDocs/Documents/PMC/"
+wd <- 
 setwd(wd)
-fwd <- paste0(wd, "PA/Analysis/Figures/") # figure working directory
 
 ########## Load Rozowsky data ##########
-
-aya_ids <- list.files(path = )
+aya_bulkFiles <- list.files(path = "/Users/filbinlab/Documents/Jacob/Data/AYA Bulk RNA-seq/")
 filePath <- c("/Users/filbinlab/Documents/Jacob/Data/AYA Bulk RNA-seq/")
-for (file in filePath) {
+
+
+for (i in 1:length(aya_bulkFiles)) {
+  file <- aya_bulkFiles[i]
   print(file)
+  sampleName <- sapply(strsplit(file,"_"), getElement, 1)
+  sampleCounts <- read.table(file = paste0(filePath, file), header = TRUE)
+  colnames(sampleCounts) <- c("Gene", sampleName)
+  
+  if (i == 1){
+    countFile <- sampleCounts
+  } else {
+    countFile <- merge(countFile, sampleCounts, by = "Gene")
+  }
 }
+
+library(biomaRt)
+mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+genes <- countFile$Gene %>% data.frame()
+colnames(genes) <- "Gene"
+genes <- genes %>% separate("Gene", c("Gene", NA)) %>% pull(Gene)
+
+G_list <- getBM(filters= "ensembl_gene_id", 
+                attributes= c("ensembl_gene_id", "hgnc_symbol", "description"),
+                values = genes,
+                mart = mart)
+
+countFile <- merge(countFile, G_list %>% select(ensembl_gene_id, ))
+
+
+library("EnsDb.Hsapiens.v86")
+symbols <- mapIds(org.Hs.eg.db, keys = genes, keytype = "ENSEMBL", column="SYMBOL")
