69e40db6f84674e21e362fa56e912324714e95e0
jrozo13 <jrozowsk@u.rochester.edu>
2022-07-13 12:05:48 -0400

    Load data file

--- LoadFiles.R
+++ LoadFiles.R
@@ -1,1 +1,32 @@
-This is a line of code
+# Bulk AYA Gliloma analysis: make datasets
+# Last updated: 13/07/2022
+
+########## Initialize ##########
+## Install global packages ##
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(readxl)
+library(RColorBrewer)
+library(circlize)
+library(ggplot2)
+library(ggrepel)
+col_annotations <- list(
+  Sex = c("M" = "skyblue", "F" = "pink"),
+  Location = c("Spinal" = "#EF553B", 
+               "Posterior fossa" = "#636EFA", 
+               "Supratentorial" = "#00CC96"),
+  Age = colorRamp2(c(0,20), c("#edf8e9", "#74c476"))
+)
+
+wd <- "/Users/jrozowsky/Library/Mobile Documents/com~apple~CloudDocs/Documents/PMC/"
+setwd(wd)
+fwd <- paste0(wd, "PA/Analysis/Figures/") # figure working directory
+
+########## Load Rozowsky data ##########
+
+aya_ids <- list.files(path = )
+filePath <- c("/Users/filbinlab/Documents/Jacob/Data/AYA Bulk RNA-seq/")
+for (file in filePath) {
+  print(file)
+}
