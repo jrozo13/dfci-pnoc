88a72ce99cbdfc316b2d5fa5183166e848371aeb
jrozo13 <jrozowsk@u.rochester.edu>
2022-07-15 14:06:22 -0400

    7/15 updates

--- LoadFiles.R
+++ LoadFiles.R
@@ -6,24 +6,8 @@
 library(tidyverse)
 
 ########## Load Rozowsky data ##########
-filePath <- c("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/")
-aya_bulkFiles <- list.files(path = filePath)
-
-for (i in 1:length(aya_bulkFiles)) {
-  file <- aya_bulkFiles[i]
-  print(file)
-  sampleName <- sapply(strsplit(file,"_"), getElement, 1)
-  sampleCounts <- read.table(file = paste0(filePath, file), header = TRUE)
-  colnames(sampleCounts) <- c("Gene", sampleName)
-  
-  sampleCounts <- sampleCounts %>% separate("Gene", c("Gene", NA))
-  
-  if (i == 1){
-    countFile <- sampleCounts
-  } else {
-    countFile <- merge(countFile, sampleCounts, by = "Gene")
-  }
-}
+filePath <- c("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-fc/")
+countFile_fc <- featurec_to_countmatrix(filePath = filePath)
 
 ### Convert from ENSEMBL to Gene Symbol ###
 source("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/dfci-pnoc/ensembl_to_symbol.R")
---
+++ bulkRNAseq-scripts.R
@@ -0,0 +1,46 @@
+##### Function to read count outputs from featureCounts #####
+featurec_to_countmatrix <- function(filePath) {
+  library(tidyverse)
+  filePath <- c("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-fc/")
+  aya_bulkFiles <- list.files(path = filePath)
+  
+  for (i in 1:length(aya_bulkFiles)) {
+    file <- aya_bulkFiles[i]
+    print(file)
+    sampleName <- sapply(strsplit(file,"_"), getElement, 1)
+    sampleCounts <- read.table(file = paste0(filePath, file), header = TRUE)
+    colnames(sampleCounts) <- c("Gene", sampleName)
+    
+    sampleCounts <- sampleCounts %>% separate("Gene", c("Gene", NA))
+    
+    if (i == 1){
+      countFile <- sampleCounts
+    } else {
+      countFile <- merge(countFile, sampleCounts, by = "Gene")
+    }
+  }
+  return(countFile)
+}
+
+
+##### Function to go from ENSEMBL to Gene Symbol #####
+ensembl_to_symbol <- function(count_matrix) {
+  library(biomaRt)
+  library(tidyverse)
+  
+  # mart was created: July 14, 2022
+  # mart <- useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+  mart <- readRDS("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/General RDS/ensembl_to_symbol.RDS")
+  
+  colnames(count_matrix)[1] <- "ensembl_gene_id"
+  G_list <- getBM(filters= "ensembl_gene_id", 
+                  attributes= c("ensembl_gene_id", "hgnc_symbol", "description"),
+                  values = count_matrix$ensembl_gene_id,
+                  mart = mart)
+  count_matrix <- merge(G_list, count_matrix, by = "ensembl_gene_id")
+  return(count_matrix)
+}
+
+
+
+
