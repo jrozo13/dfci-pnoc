3fa0f251cde0a2c0be01b1f08dd6637284e165f8
jrozo13 <jrozowsk@u.rochester.edu>
2022-07-15 14:23:08 -0400

    Updates to bulk analysis

--- AYAGlioma.R
+++ AYAGlioma.R
@@ -1,14 +1,17 @@
-# Bulk AYA Gliloma analysis: make datasets
-# Last updated: 13/07/2022
+# Bulk AYA Gliloma analysis
+# Last updated: 15/07/2022
 
 ########## Initialize ##########
 ## Install global packages ##
 library(tidyverse)
 source("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/dfci-pnoc/bulkRNAseq-scripts.R")
 
-########## Load Rozowsky data ##########
+########## Load AYA data ##########
 filePath <- c("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-fc/")
 countFile_fc <- featurec_to_countmatrix(filePath = filePath)
 
+countFile_rsem <- rsem_to_countmatrix(filePath = "/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-rsem/")
+
 ### Convert from ENSEMBL to Gene Symbol ###
-countFile <- ensembl_to_symbol(count_matrix = countFile)
+countFile_fc <- ensembl_to_symbol(count_matrix = countFile_fc)
+countFile_rsem <- ensembl_to_symbol(count_matrix = countFile_rsem)
--- bulkRNAseq-scripts.R
+++ bulkRNAseq-scripts.R
@@ -55,11 +55,15 @@ ensembl_to_symbol <- function(count_matrix) {
   mart <- readRDS("/Users/filbinlab/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/General RDS/ensembl_to_symbol.RDS")
   
   colnames(count_matrix)[1] <- "ensembl_gene_id"
-  G_list <- getBM(filters= "ensembl_gene_id", 
+  G_list <- getBM(filters = "ensembl_gene_id", 
                   attributes= c("ensembl_gene_id", "hgnc_symbol", "description"),
                   values = count_matrix$ensembl_gene_id,
                   mart = mart)
   count_matrix <- merge(G_list, count_matrix, by = "ensembl_gene_id")
+  count_matrix <- count_matrix %>% 
+    subset(select = -c(ensembl_gene_id, description)) %>%
+    filter(hgnc_symbol != "")
+  
   return(count_matrix)
 }
 
