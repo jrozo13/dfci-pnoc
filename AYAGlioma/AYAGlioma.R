e5fc518b787dbc1db4cc28aefbe1a1274254be73
jrozo13 <jrozowsk@u.rochester.edu>
2022-07-21 10:37:34 -0400

    Bulk RNA-seq analysis updates

--- AYAGlioma/AYAGlioma.R
+++ AYAGlioma/AYAGlioma.R
@@ -12,32 +12,40 @@ library(reshape2)
 ## Install custom functions
 source("~/Dropbox (Partners HealthCare)/Filbin lab/Jacob/dfci-pnoc/bulkRNAseq-scripts.R")
 
-
 ########## Load Data ##########
 ##### Load bulk RNAseq Data
 filePath_fc <- c("~/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-fc/")
-countFile_fc <- featurec_to_countmatrix(filePath = filePath_fc)
+countFile_fc <- MakeCountMatrixFromFeatureCounts(filePath = filePath_fc)
 
 filePath_rsem <- c("~/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-rsem/")
-countFile_rsem <- rsem_to_countmatrix(filePath = filePath_rsem)
+countFile_rsem <- MakeCountMatrixFromRSEM(filePath = filePath_rsem)
 
-### Convert from ENSEMBL to Gene Symbol ###
-countFile_fc <- ensembl_to_symbol(count_matrix = countFile_fc)
+countFile_fc <- Ensembl2Symbol(count_matrix = countFile_fc)
 countFile_fc <- countFile_fc %>% column_to_rownames(var = "hgnc_symbol")
 
-countFile_rsem <- ensembl_to_symbol(count_matrix = countFile_rsem)
+countFile_rsem <- Ensembl2Symbol(count_matrix = countFile_rsem)
 countFile_rsem <- countFile_rsem %>% column_to_rownames(var = "hgnc_symbol")
 
 shared_genes <- intersect(rownames(countFile_fc), rownames(countFile_rsem))
 
+##### Load QC Files
+filePath_qc <- c("~/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/star-fc/star-fc.QC/")
+qcFile_fc <- MakeQCFileFromFeatureCountsOutput(filePath.QC = filePath_qc)
+
 ##### Load AYA clinical data
 meta <- read_excel("~/Dropbox (Partners HealthCare)/Filbin lab/Jacob/Data/AYA Bulk RNA-seq/Deidentified RNAseq Patient Clinical Data.xlsx",
-                           skip = 1) 
+                   skip = 1) 
 meta <- meta %>%
   mutate(SampleID.new = paste0("s", sapply(strsplit(as.character(meta$SampleID), "-"), `[`, 1))) %>% 
   # only keeps sample ID before "-"
   filter(SampleID.new %in% colnames(countFile_fc)) %>%
-  filter(SampleID != "94410-1*")
+  filter(SampleID != "94410-1*") %>%
+  mutate(time = as.numeric(`Overall Survival (Years)`)) %>% 
+  mutate(pathology16 = `Pathology at Diagnosis`) %>%
+  mutate(pathology21 = ifelse(`Pathology at Diagnosis` %in% c("Anaplastic Astrocytoma, WHO Grade III, IDH-Mutant", "Diffuse Astrocytoma, WHO Grade II, IDH-Mutant"), "Astrocytoma, IDH-Mutant",
+                              ifelse(`Pathology at Diagnosis` == "Oligodendroglioma, WHO Grade II", "Oligodendroglioma, IDH-Mutant", "Glioblastoma"))) %>%
+  mutate(status = ifelse(`Vital Status` == "Alive", 1, 2)) # this will be used in `status` in survival package
+
 # check
 meta$SampleID.new %in% colnames(countFile_fc) %>% table()
 colnames(countFile_fc) %in% meta$SampleID.new %>% table()
@@ -59,7 +67,7 @@ pheatmap(fc.rsem_Cor, cluster_rows = F, cluster_cols = F)
 densityDF <- data.frame(sum = c(colSums(countFile_fc), colSums(countFile_rsem)), method = c(rep("featureCounts", 23), rep("rsem", 23)))
 ggplot(densityDF, aes(x = sum, fill = method)) +
   geom_density(alpha = 0.4) +
-  xlim(min-10^7/2, max+10^7/2) +
+  xlim(c(min-10^7/2, max+10^7/2)) +
   xlab(label = "Total gene expression")
 
 cpm_fc <- apply(countFile_fc, 2, function(x) (x/sum(x))*1000000)
@@ -68,12 +76,44 @@ for (sample in colnames(cpm_fc)) {
   print(sample)
   print(cor(cpm_fc[shared_genes, sample], cpm_rsem[shared_genes, sample]))
 }
+# Use featureCounts output from here on out
+cpm <- cpm_fc
+
+rm(cpm_rsem)
+rm(countFile_rsem)
+rm(filePath_rsem)
+
+##### Qualtiy control on featureCounts data
+qcPlots <- MakeQCPlotsFromQCData(qcFile = qcFile_fc, countMatrix = cm)
+qcPlots[[1]]
+qcPlots[[2]]
+qcPlots[[3]]
+
+########## Survival on clinical metadata ##########
+library(survival)
+library(survminer)
+
+fit <- survfit(Surv(time = time, status) ~ pathology16, data = meta)
+pvalue <- surv_pvalue(fit, meta)$pval.txt
+ggsurvplot(fit,
+           pval = TRUE,
+           risk.table = TRUE, 
+           risk.table.y.text = FALSE,
+           risk.table.height = 0.25,
+           legend.labs = c("Anaplastic Astro.", "Diffuse Astro.", "Glioblastoma", "Oligodendroglioma"))
 
 ########## Score sample by expression ##########
-input <- NormCenter(cm = cpm_fc)
-input.mean <- rowMeans(log2(input$raw_data+1))
-meta$gluta <- scoreSignature(input$center_data, input.mean, s = syn_genesets$curatedSynaptic$gluta, simple = FALSE, verbose = TRUE)
+input <- CenterCountsMatrix(cm = cpm)
+meta$geneSig <- scoreSignature(input$cm_center, input$cm_mean, s = syn_genesets$curatedSynaptic$gluta, simple = FALSE, verbose = TRUE)
+ggplot(meta, aes(x = pathology16, y = geneSig)) +
+  geom_boxplot() +
+  geom_jitter()
 
+meta$gaba <- scoreSignature(input$cm_center, input$cm_mean, s = syn_genesets$curatedSynaptic$gaba, simple = FALSE, verbose = TRUE)
+meta$general_synaptic <- scoreSignature(input$cm_center, input$cm_mean, s = syn_genesets$curatedSynaptic$general_synaptic, simple = FALSE, verbose = TRUE)
+intersect(syn_genesets$curatedSynaptic$gaba, syn_genesets$curatedSynaptic$gluta) # these gene sets do not overlap
+plot(meta$gluta, meta$gaba)
+hist(meta$gluta)
 RunSurvivalAnalysis()
 
 
--- bulkRNAseq-scripts.R
+++ bulkRNAseq-scripts.R
@@ -1,7 +1,7 @@
 ##### Function to read count outputs from featureCounts #####
-featurec_to_countmatrix <- function(filePath) {
+MakeCountMatrixFromFeatureCounts <- function(filePath) {
   library(tidyverse)
-  files <- list.files(path = filePath)
+  files <- list.files(path = filePath, pattern = ".txt")
   
   for (i in 1:length(files)) {
     file <- files[i]
@@ -22,9 +22,9 @@ featurec_to_countmatrix <- function(filePath) {
 }
 
 ##### Function to read count outputs from rsem #####
-rsem_to_countmatrix <- function(filePath) {
+MakeCountMatrixFromRSEM <- function(filePath) {
   library(tidyverse)
-  files <- list.files(path = filePath)
+  files <- list.files(path = filePath, pattern = ".txt")
   
   for (i in 1:length(files)) {
     file <- files[i]
@@ -44,9 +44,71 @@ rsem_to_countmatrix <- function(filePath) {
   return(countFile)
 }
 
+##### Function to make QC file from featureCounts.summary outputs #####
+MakeQCFileFromFeatureCountsOutput <- function(filePath.QC) {
+  library(tidyverse)
+  files <- list.files(path = filePath.QC, pattern = ".txt.summary")
+  for (i in 1:length(files)) {
+    file <- files[i]
+    print(file)
+    sampleName <- sapply(strsplit(file,"_"), getElement, 1)
+    sampleQC <- read.table(file = paste0(filePath.QC, file), header = TRUE)
+    colnames(sampleQC) <- c("Metric", sampleName)
+    if (i == 1){
+      qcFile <- sampleQC
+    } else {
+      qcFile <- merge(qcFile, sampleQC, by = "Metric")
+    }
+  }
+  qcFile <- qcFile %>% column_to_rownames(var = "Metric")
+  qcFile <- data.frame(t(qcFile))
+  qcFile$Unassigned_All <- rowSums(qcFile) - qcFile$Assigned
+  return(qcFile)
+}
+
+##### Function to make QC plots from qcFile and count matrix #####
+MakeQCPlotsFromQCData <- function(qcFile, countMatrix) {
+  library(readr)
+  riboGenes <- read_csv("~/Dropbox (Partners HealthCare)/Filbin lab/Shared/Marker_genes/RibosomalGenes.csv", col_names = "Gene", show_col_types = FALSE) %>% pull(Gene)
+  nRiboCounts <- countMatrix[rownames(countMatrix) %in% riboGenes,]
+  riboQC <- data.frame(RiboPercent = colSums(nRiboCounts)/colSums(countMatrix), CountSum = colSums(countMatrix))
+  qcFile <- cbind(qcFile, riboQC) %>% rownames_to_column(var = "sample")
+  
+  plot1 <- ggplot(qcFile, aes(x=sample, y=RiboPercent))+ 
+    geom_bar(position="dodge", stat="identity")+
+    ylim(0, 0.25)+
+    xlab("")+
+    ylab("Percent of counts")+
+    ggtitle("Percentage of ribosomal counts") +
+    theme(axis.text=element_text(color = "black", face = "bold"),
+          axis.title.y = element_text(color = "black", face = "bold", size = 12),
+          axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  plot2 <- ggplot(qcFile, aes(x = sample, y = Assigned))+ 
+    geom_bar(position="dodge", stat="identity")+
+    geom_hline(yintercept = 10^7, color="red", linetype="dashed", size=1.5)+
+    xlab("")+
+    ylab("Number of assigned reads")+
+    ggtitle("Number of reads")+
+    theme(axis.text=element_text(color="black", face="bold"),
+          axis.title.y = element_text(color="black", face="bold", size=12),
+          axis.text.x = element_text(angle=45, hjust=1))
+  
+  plot3 <- ggplot(qcFile, aes(x = sample, y = Unassigned_All/(Unassigned_All + Assigned)))+ 
+    geom_bar(position="dodge", stat="identity")+
+    xlab("")+
+    ylim(c(0,1)) +
+    ylab("Percentage of total reads")+
+    ggtitle("Percentage of unassigned reads")+
+    theme(axis.text=element_text(color="black", face="bold"),
+          axis.title.y = element_text(color="black", face="bold", size=12),
+          axis.text.x = element_text(angle=45, hjust=1))
+  
+  return(list(plot1, plot2, plot3))
+}
 
 ##### Function to go from ENSEMBL to Gene Symbol #####
-ensembl_to_symbol <- function(count_matrix) {
+Ensembl2Symbol <- function(count_matrix) {
   library(biomaRt)
   library(tidyverse)
   
@@ -69,15 +131,19 @@ ensembl_to_symbol <- function(count_matrix) {
 }
 
 ##### Function to make input count matrix for ScoreSignature #####
-NormCenter<-function(cm, scale_factor=10, log_base=2){
-  cm = as.matrix(cm)
-  cm_norm = log(cm/scale_factor+1, base=log_base)
-  cm_norm_center = cm_norm-rowMeans(cm_norm)
-  result = list()
-  result[["raw_data"]] = cm
-  result[["norm_data"]] = cm_norm
-  result[["center_data"]] = cm_norm_center
-  return (result)
+CenterCountsMatrix<- function(cm, alreadyCentered=FALSE){
+  cm_list<- list()
+  if(alreadyCentered){
+    cm_list$tpm<- NA
+    cm_list$cm_center<- cm
+    cm_list$cm_mean<- rowMeans(cm_list$cm_center)
+  } else{
+    normCenter<- NormCenter(cm)
+    cm_list$tpm<- cm
+    cm_list$cm_center<- normCenter$center_data
+    cm_list$cm_mean<- rowMeans(log2(cm_list$tpm + 1))
+  }
+  return(cm_list)
 }
 
 ##### Function to score bulk RNA-seq samples with gene-set #####
@@ -85,28 +151,25 @@ NormCenter<-function(cm, scale_factor=10, log_base=2){
 ## @param X.mean average of relative expression of each gene (log2 transformed)
 ## @param n number of genes with closest average expression for control genesets, default = 100 
 ## @param simple whether use average, default  = FALSE
-scoreSignature <- function(X.center, X.mean, s, n=100, simple = FALSE, verbose=FALSE) {
+scoreSignature <- function(cm.center, cm.mean, s, n=100, simple = FALSE, verbose=FALSE) {
   if(verbose) {
-    message("cells: ", ncol(X.center))
-    message("genes: ", nrow(X.center))
+    message("cells: ", ncol(cm.center))
+    message("genes: ", nrow(cm.center))
     message("genes in signature: ", length(s))
     message("Using simple average?", simple)
     message("processing...")
   }
   
-  s <- intersect(rownames(X.center), s)
+  s <- intersect(rownames(cm.center), s)
   message("genes in signature, and also in this dataset: ", length(s))
-  ##message("These genes are: ", s)
-  
+
   if (simple){
-    s.score <- colMeans(X.center[s,])
-  }else{
+    s.score <- colMeans(v[s,])
+  } else {
     s.score <- colMeans(do.call(rbind, lapply(s, function(g) {
-      # g <- s[2]
-      # message(g)
       if(verbose) message(".", appendLF = FALSE)
-      g.n <- names(sort(abs(X.mean[g] - X.mean))[2:(n+1)])
-      X.center[g, ] - colMeans(X.center[g.n, ])
+      g.n <- names(sort(abs(cm.mean[g] - cm.mean))[2:(n+1)])
+      cm.center[g, ] - colMeans(cm.center[g.n, ])
     })))
   }
   
@@ -114,4 +177,37 @@ scoreSignature <- function(X.center, X.mean, s, n=100, simple = FALSE, verbose=F
   return(s.score)
 }
 
-
+## From a dataframe of scores for each geneset, split samples into high/low expressors
+## For each module score, split samples into "high" or "low" expression
+## Input: scores = a dataframe with genesets as columns, samples as rows 
+##        splitBy = denotes how to split samples into high/low geneset scorers
+SplitHighLow<- function(scores, splitBy="Median"){
+  AllClusters<-data.frame(submitter_id=Scores$submitter_id)
+  for (m in names(marker_list_InCohort)){
+    print(m)
+    df<- cbind(Scores[,"submitter_id"], Scores[,m]); df<-as.data.frame(df)
+    ClusterName<- paste0(m, "_Cluster")
+    df$V2<-as.numeric(df$V2)
+    
+    ## Alternative methods for splitting into high/low
+    ## Split into top25%/mid50%/bottom25%
+    if(SplitBasedOn=="SplitInto3Quartiles"){
+      df[,"ClusterName"]<- df$V2>quantile(df$V2)["75%"]
+      high<- df[df$V2>quantile(df$V2)["75%"],]; high[,"ClusterName"]<-"High"
+      low<- df[df$V2<quantile(df$V2)["25%"],]; low[,"ClusterName"]<-"Low"
+      mid<-df[!(df$V1 %in% c(high$V1, low$V1)),]; mid[,"ClusterName"]<-"Mid"
+      df<-rbind(high,low); df<-rbind(df, mid)
+    } 
+    ## Split based on median
+    if(SplitBasedOn=="Median"){
+      df[,"ClusterName"]<- df$V2>median(df$V2)
+      df[,"ClusterName"]<- gsub("FALSE", "Low", 
+                                gsub("TRUE", "High", df[,"ClusterName"]))
+    } 
+    
+    ## Convert to factor, with "Low" as comparison
+    #df$ClusterName<- factor(df$ClusterName, levels=c("High", "Low"))
+    colnames(df)<- c("submitter_id", m, ClusterName)
+    AllClusters<-merge(AllClusters,df, by="submitter_id")
+  }
+}
